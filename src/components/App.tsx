b13b33f85c1a0f40a15cfcad5c04aeea94e2ca4a
Shamp07 <blog.shamp@gmail.com>
2021-07-01 22:39:33 +0900

    feat: initialize development environment

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Hello, Typescript!
+  </div>
+);
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: Configuration = {
+  mode: 'development',
+  entry: './src',
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
+    }),
+  ],
+};
+
+export default config;
